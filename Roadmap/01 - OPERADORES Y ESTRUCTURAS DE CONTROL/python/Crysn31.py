3a5637f3d0407e049d106c8031a765ba7c812b8a
Crysn31 <luiscuadros0614@gmail.com>
2026-02-17 16:52:05 -0500

    Crysn31.py

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/Crysn31.py
@@ -0,0 +1,49 @@
+""" operadores """
+# operador aritmetico
+print(f"Suma: 10 + 3 = { 10 + 3}")
+print(f"Resta: 10 - 3 = { 10 - 3}")
+print(f"Multiplicacion: 10 * 3 = { 10 * 3}")
+print(f"Division: 10 / 3 = { 10 / 3}")
+print(f"Modulo: 10 % 3 = { 10 % 3}")
+print(f"Exponente: 10 ** 3 = {10 ** 3}")
+print(f"Division entera : 10 // 3 = {10 // 3}")
+
+#operadores de comparacion 
+
+print(f"igualdad: 10 == 3 es{10 == 3}")
+print(f"Desigualdad: 10 != 3 es {10!= 3}")
+print(f"Mayor que: 10 > 3 es {10 > 3}")
+print(f"Menor que: 10 < 3 es {10 < 3}")
+print(f"Mayor o igual que: 10 >= 3 es {10 >= 3}")
+print(f"Menor o igual que: 10 <= 3 es {10 <= 3}")
+
+#operadores logicos
+print(f"AND: 10 + 3 == 13 and 5 - 1 == 4 es { 10 + 3 == 13 and 5 - 1 == 4 }" )
+print(f"OR: 10 + 3 == 13 or 5 - 1 == 4 es {10 + 3 == 14  or 5 - 1 == 4}")
+print(f"NOT: not 10 + 3 == 14 es {not 10 + 3 == 14}")
+
+# Operadores de asignacion
+
+my_number = 11 # asignacion
+print (my_number) 
+my_number += 1 # suma y asignacion
+print (my_number) 
+my_number -= 1 # resta y asignacion
+print (my_number) 
+my_number *= 2 # Multiplicacion y asignacion
+print (my_number) 
+my_number /= 2 # division y asignacion
+print (my_number) 
+my_number %= 2 # modulo y asignacion
+print (my_number) 
+my_number **= 2 # exponente y asignacion
+print (my_number) 
+my_number //= 2 # diviison entera y asignacion
+print (my_number) 
+
+# Operadores de identidad
+
+my_new_number = my_number
+print(f"my_number is my_new_number es {my_number is my_new_number}")
+print(f"my_number is not my_new_number es {my_number is not my_new_number}")
+ 
