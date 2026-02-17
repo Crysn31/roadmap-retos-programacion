543b9e8640edb2f67f4c84257e38953f3cf83269
Crysn31 <luiscuadros0614@gmail.com>
2026-02-17 15:02:32 -0500

    Crysn31.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Crysn31.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Crysn31.py
@@ -1,1 +1,25 @@
+# https://python.org
+# cometario en una linea
+""" Omegalul estoy aprendiendo a pogramar """
+''' Rubius el mas chepon '''
 
+my_variable = "MI VARIABLE"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convencion
+MY_CONSTANT = "JUAN"
+
+my_int = 1
+my_float = 3,5
+my_bool = False
+my_string = "mi cadena de texto"
+my_other_string = 'mi otra'
+
+print (" !Hola, Python! ")
+
+print (type(my_int))
+print (type(my_float))
+print (type(my_bool))
+print (type(my_string))
+
+      
